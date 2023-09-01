0f23de08ae815977908eb270fa81a762e59f551b
Dawid <109389692+DawidBartus@users.noreply.github.com>
2023-09-01 17:58:51 +0200

    flappy tomato

--- flappy_tomato/index.js
+++ flappy_tomato/index.js
@@ -7,8 +7,9 @@ class Game {
     posY = 240;
     gravity = 1.1;
     score = 0;
+
     pipes = [];
-    pipeGap = 120;
+    pipesGap = 120;
 
     init = () => {
         this.canvas = document.getElementById('canvas');
@@ -21,7 +22,7 @@ class Game {
         this.bg.src = 'images/1.png';
 
         this.pipeTop = new Image();
-        this.pipeTop.src = 'images/pipeBottom.png';
+        this.pipeTop.src = 'images/pipeTop.png';
 
         this.pipeBottom = new Image();
         this.pipeBottom.src = 'images/pipeBottom.png';
@@ -59,7 +60,7 @@ class Game {
             bottom: {
                 img: this.pipeBottom,
                 x: x,
-                y: y + this.pipeTop.height + this.pipeGap,
+                y: y + this.pipeTop.height + this.pipesGap,
                 width: this.pipeBottom.width,
                 height: this.pipeBottom.height,
             },
@@ -67,12 +68,54 @@ class Game {
     };
 
     updateGame = () => {
-        this.render();
         this.addGravity();
+        this.checkCollision();
+
+        this.render();
     };
 
     addGravity = () => {
         this.posY += this.gravity;
+    };
+
+    checkCollision = () => {
+        if (this.posY > this.canvas.height - this.tomato.height) {
+            this.moveUp();
+        }
+
+        if (this.posY < 0) {
+            this.posY = 0;
+        }
+
+        const pipesToCheck = [...this.pipes];
+
+        const bX = this.posX;
+        const bY = this.posY;
+        const tW = this.tomato.width;
+        const tH = this.tomato.height;
+
+        pipesToCheck.forEach((pipe) => {
+            if (bX + tW > pipe.top.x && bX <= pipe.top.x + pipe.top.width) {
+                if (
+                    bY < pipe.top.y + pipe.top.height ||
+                    bY + tH > pipe.bottom.y
+                ) {
+                    this.restart();
+                }
+            }
+
+            if (pipe.top.x === -1) {
+                this.score++;
+
+                if (this.score % 2 === 0) {
+                    this.pipesGap--;
+
+                    if (this.pipesGap === 110) {
+                        this.pipesGap = 110;
+                    }
+                }
+            }
+        });
     };
 
     render = () => {
@@ -83,8 +126,19 @@ class Game {
         this.context.drawImage(this.tomato, this.posX, this.posY);
 
         this.context.fillStyle = '#fff';
-        this.context.font = '20px verdana';
+        this.context.font = '20px Verdana';
         this.context.fillText(`Score: ${this.score}`, 20, 25);
+    };
+
+    restart = () => {
+        this.posX = 30;
+        this.posY = 240;
+        this.score = 0;
+
+        this.pipes = [];
+        this.pipesGap = 120;
+
+        this.addPipe();
     };
 
     drawPipes = () => {
